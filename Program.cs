a5dbb97a1dc2a16cd0e88af27bc93dd4a99529b6
Your Name <you@example.com>
2020-02-25 16:27:49 +1100

    reads and prints first name

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your first name?");
+            string firstname = Console.ReadLine();
+
+            Console.writeline("whats your surname?");
+            string surname = Console.ReadLine();
+
+            string fullname = firstname + " " + surname;
+
+            Console.WriteLine(fullname);
+        }
+    }
+}
